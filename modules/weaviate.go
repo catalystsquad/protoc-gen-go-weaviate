ee625cfc5715b5b92a6b0d1ad6d367646544cf51
zack stevens <zack.st7@gmail.com>
2023-05-09 14:52:54 -0600

    in progress commit

--- modules/weaviate.go
+++ modules/weaviate.go
@@ -2,6 +2,7 @@ package modules
 
 import (
 	"fmt"
+	"github.com/iancoleman/strcase"
 	pgsgo "github.com/lyft/protoc-gen-star/v2/lang/go"
 	"text/template"
 
@@ -21,9 +22,12 @@ func (p *WeaviateModule) InitContext(c pgs.BuildContext) {
 	p.ctx = pgsgo.InitContext(c.Parameters())
 
 	tpl := template.New("weaviate").Funcs(map[string]interface{}{
-		"className": p.getClassName,
-		"dataType":  p.getPropertyDataType,
-		"name":      p.getPropertyName,
+		"structName":       p.getStructName,
+		"structFieldName":  p.getStructFieldName,
+		"structFieldType":  p.getStructFieldType,
+		"className":        p.getClassName,
+		"propertyName":     p.getPropertyName,
+		"propertyDataType": p.getPropertyDataType,
 	})
 
 	p.tpl = template.Must(tpl.Parse(WeaviateTemplate))
@@ -31,6 +35,10 @@ func (p *WeaviateModule) InitContext(c pgs.BuildContext) {
 
 func (p *WeaviateModule) getClassName(m pgs.Message) string {
 	return m.Name().String()
+}
+
+func (p *WeaviateModule) getStructName(m pgs.Message) string {
+	return p.getClassName(m)
 }
 
 func (p *WeaviateModule) getCrossReferenceType(field pgs.Field) string {
@@ -41,8 +49,35 @@ func (p *WeaviateModule) getCrossReferenceType(field pgs.Field) string {
 	return field.Type().Element().Embed().Name().String()
 }
 
+func (p *WeaviateModule) getStructFieldStructType(field pgs.Field) string {
+	embed := field.Type().Embed()
+	if embed != nil {
+		return embed.Name().String()
+	}
+	return field.Type().Element().Embed().Name().String()
+}
+
 func (p *WeaviateModule) getNonCrossReferenceType(field pgs.Field) string {
-	return typeMap[field.Type().ProtoType()]
+	return weaviateTypeMap[field.Type().ProtoType()]
+}
+
+func (p *WeaviateModule) getNonStructFieldType(field pgs.Field) string {
+	return goTypeMap[field.Type().ProtoType()]
+}
+
+func (p *WeaviateModule) getStructFieldType(field pgs.Field) (datatype string) {
+	if isStructType(field) {
+		datatype = p.getStructFieldStructType(field)
+	} else {
+		datatype = p.getNonStructFieldType(field)
+	}
+	if isList(field) {
+		datatype = fmt.Sprintf("[]%s", datatype)
+	}
+	if isPointer(field) {
+		datatype = fmt.Sprintf("*%s", datatype)
+	}
+	return
 }
 
 func (p *WeaviateModule) getPropertyDataType(field pgs.Field) (dataType string) {
@@ -61,15 +96,25 @@ func isCrossReference(field pgs.Field) bool {
 	return field.Type().ProtoType() == pgs.MessageT
 }
 
+func isStructType(field pgs.Field) bool {
+	return field.Type().ProtoType() == pgs.MessageT
+}
+
+func isPointer(field pgs.Field) bool {
+	label := field.Descriptor().Proto3Optional
+	return label != nil && *label
+}
+
 func isList(field pgs.Field) bool {
 	return field.Type().IsRepeated()
 }
 
-func getFieldProtoType(field pgs.Field) pgs.ProtoType {
-	return field.Type().ProtoType()
-}
 func (p *WeaviateModule) getPropertyName(field pgs.Field) string {
 	return field.Name().String()
+}
+
+func (p *WeaviateModule) getStructFieldName(field pgs.Field) string {
+	return strcase.ToCamel(p.getPropertyName(field))
 }
 
 func (p *WeaviateModule) Name() string { return "weaviate" }
@@ -104,21 +149,32 @@ import (
   "github.com/weaviate/weaviate/entities/models"
 )
 {{ range .AllMessages }}
-var {{ className . }}Class = models.Class{
-  Class:       "{{ className . }}",
-  Properties: []*models.Property{
-  {{- range .Fields }}
-    {
-      DataType:    []string{"{{ dataType . }}"},
-      Name:        "{{ name . }}",
-    },
-    {{- end }}
-  },
+type {{ structName . }} struct {
+	{{ range .Fields }}
+	{{- structFieldName . }} {{ structFieldType . }}
+	{{ end -}}
 }
+
+func ({{ structName . }}) WeaviateClassName() string {
+	return "{{ className . }}"
+}
+
+func ({{ structName . }}) WeaviateModelDefinition() models.Model {
+	return models.Class{
+  		Class:       "{{ className . }}",
+  		Properties: []*models.Property{
+  		{{- range .Fields }}
+			{
+			  Name:        "{{ propertyName . }}",
+			  DataType:    []string{"{{ propertyDataType . }}"},
+			},
+    	{{- end }}
+  	}
+} 
 {{ end }}
 `
 
-var typeMap = map[pgs.ProtoType]string{
+var weaviateTypeMap = map[pgs.ProtoType]string{
 	pgs.StringT:  "text",
 	pgs.EnumT:    "text",
 	pgs.BoolT:    "boolean",
@@ -135,4 +191,23 @@ var typeMap = map[pgs.ProtoType]string{
 	pgs.SFixed64: "int",
 	pgs.SInt32:   "int",
 	pgs.SInt64:   "int",
+}
+
+var goTypeMap = map[pgs.ProtoType]string{
+	pgs.StringT:  "string",
+	pgs.EnumT:    "int",
+	pgs.BoolT:    "bool",
+	pgs.BytesT:   "[]byte",
+	pgs.DoubleT:  "float64",
+	pgs.FloatT:   "float64",
+	pgs.Int64T:   "int64",
+	pgs.UInt64T:  "uint64",
+	pgs.Int32T:   "int32",
+	pgs.Fixed64T: "int64",
+	pgs.Fixed32T: "int32",
+	pgs.UInt32T:  "uint32",
+	pgs.SFixed32: "int32",
+	pgs.SFixed64: "int64",
+	pgs.SInt32:   "int32",
+	pgs.SInt64:   "int64",
 }
